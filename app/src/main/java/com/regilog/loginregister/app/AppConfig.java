4d5daebe4928089c59d8fec0d52e72ae3c4c2b1c
ramon58 <ramoncarrion@gmail.com>
2016-12-01 16:23:53 +0100

    Project

---
+++ app/src/main/java/com/regilog/loginregister/app/AppConfig.java
@@ -0,0 +1,9 @@
+package com.regilog.loginregister.app;
+
+public class AppConfig {
+	// Server user login url
+	public static String URL_LOGIN = "http://www.iskuggan.com/android_login_api_dos/login.php";
+
+	// Server user register url
+	public static String URL_REGISTER = "http://www.iskuggan.com/android_login_api_dos/register.php";
+}
